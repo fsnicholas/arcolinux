b2a55af47e2466e0c8167ee643b6db670b250a59
Frank Nicholas <frank.nicholas@gmail.com>
2020-07-08 11:31:01 +0930

    change directoy to fsn-mirrors

---
+++ init-git.sh
@@ -0,0 +1,7 @@
+git init
+git config --global user.name "Frank Nicholas"
+git config --global user.email "frank.nicholas@gmail.com"
+sudo git config --system core.editor code
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=18000'
+git config --global push.default simple
